b97e0ea4fb0c3989e973e02cc47cd8d34911c752
Madhuvishali24 <118181567+Madhuvishali24@users.noreply.github.com>
2023-02-26 09:46:37 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,12 @@
+def fun_name(num1,num2,operator):
+    if operator=="+":
+        print(num1=num2)
+    elif operator=="-":
+        print(num1-num2)
+    elif operator=="*":
+        print(num1*num2)
+    elif operator=="/":
+        print(num1/num2)
+fun_name(24,67,"-")
+
+
